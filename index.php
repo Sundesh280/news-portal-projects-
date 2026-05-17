d73bc22b45f97da1f727514862b064bd96d0fe7d
Sundesh280 <albertghimire280@gmail.com>
2026-05-17 13:11:41 +0545

    midfied index

--- index.php
+++ index.php
@@ -160,6 +160,7 @@ session_start(); ?>
   <script src="js/auth.js"></script>
   <script src="js/main.js"></script>
   <script src="js/article.js"></script>
+  <script src="js/lang-toggle.js"></script>
   <script src="js/air-pollution.js"></script>
 </body>
 
